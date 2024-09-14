4212e34a71e9406ea1229a7a9283fd809c5682ee
22051988Di <vitalii.soloninko@gmail.com>
2024-09-14 13:21:59 +0200

    245.2 Save value to localStorage adn update 0,5 sec

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -6,6 +6,7 @@ import {
 	viewChild,
 } from '@angular/core'
 import { FormsModule, NgForm } from '@angular/forms'
+import { debounceTime } from 'rxjs'
 
 @Component({
 	selector: 'app-login',
@@ -20,9 +21,17 @@ export class LoginComponent {
 
 	constructor() {
 		afterNextRender(() => {
-			const subscription = this.form().valueChanges?.subscribe({
-				next: value => console.log(value),
-			})
+			const subscription = this.form()
+				.valueChanges?.pipe(debounceTime(500))
+				.subscribe({
+					next: value =>
+						window.localStorage.setItem(
+							'saved-login-form',
+							JSON.stringify({
+								email: value.email,
+							})
+						),
+				})
 			this.destroyRef.onDestroy(() => subscription?.unsubscribe())
 		})
 	}
